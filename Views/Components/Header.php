97c102f9503ae378d3e42eb76fe2002effb59d18
quanha567 <77888463+quanha567@users.noreply.github.com>
2022-06-12 14:16:22 +0700

    Add files via upload

--- Views/Components/Header.php
+++ Views/Components/Header.php
@@ -9,6 +9,11 @@
     <link rel="stylesheet" href="./Assets/Styles/app.css?v=<?php echo time(); ?>">
     <script type="module" src="https://unpkg.com/ionicons@5.5.2/dist/ionicons/ionicons.esm.js"></script>
     <script nomodule src="https://unpkg.com/ionicons@5.5.2/dist/ionicons/ionicons.js"></script>
+    <link
+        rel="stylesheet"
+        type="text/css"
+        href="https://cdn.jsdelivr.net/npm/slick-carousel@1.8.1/slick/slick.css"
+      />
 </head>
 <body>
     <div class="app">
@@ -17,7 +22,7 @@
                 <div class="header-top">
                    <div class="header-top-left">
                         <div class="logo">
-                            FulllStacks
+                            FullStack
                         </div>
                         <?php include_once('Navbar.php') ?>
                    </div>
@@ -26,10 +31,10 @@
                             <input type="text" placeholder="Tìm kiếm khóa học">
                             <button><ion-icon name="search-outline"></ion-icon></button>
                         </form>
-                        <div class="login">
+                        <a class="login" href="#">
                             <ion-icon name="person-outline"></ion-icon>
                             <span class="login-text">Đăng nhập</span>
-                        </div>
+                        </a>
                    </div>
                 </div>
                 <?php include_once('Slider.php') ?>
---
+++ Views/Components/Slider.php
@@ -0,0 +1,7 @@
+<div class="slider">
+    <div class="slider-list">
+        <div class="slider-items">
+            <img src="./" alt="" class="slider-img">
+        </div>
+    </div>
+</div>
