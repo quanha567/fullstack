5a64131c78406cf2a2e7fa9356bfaced5db771f5
quanha567 <77888463+quanha567@users.noreply.github.com>
2022-06-13 02:48:44 +0700

    Add files via upload

--- Views/Components/Footer.php
+++ Views/Components/Footer.php
@@ -1,9 +1,57 @@
-<footer class="footer"></footer>
+<footer class="footer">
+    <div class="center">
+        <div class="term">
+            <div class="term-container">
+                <div class="term-box">
+                    <h2 class="term-title">Học lập trình để đi làm</h2>
+                    <ul class="term-list">
+                        <li class="term-items"><a href="#" class="term-link">Điện thoại: 0246.329.1102</a></li>
+                        <li class="term-items"><a href="#" class="term-link">Email: contact@fullstack.edu.vn</a></li>
+                        <li class="term-items"><a href="#" class="term-link">Địa chỉ: 41 đường 30, Linh Đông, Thủ Đức, TP.Hồ Chí Minh</a></li>
+                    </ul>
+                </div>
+                <div class="term-box">
+                    <h2 class="term-title">Về Fullstack</h2>
+                    <ul class="term-list">
+                        <li class="term-items"><a href="#" class="term-link">Giới thiệu</a></li>
+                        <li class="term-items"><a href="#" class="term-link">Cơ hội việc làm</a></li>
+                        <li class="term-items"><a href="#" class="term-link">Đối tác</a></li>
+                    </ul>
+                </div>
+                <div class="term-box">
+                    <h2 class="term-title">Hỗ Trợ</h2>
+                    <ul class="term-list">
+                        <li class="term-items"><a href="#" class="term-link">Liên hệ</a></li>
+                        <li class="term-items"><a href="#" class="term-link">Bảo mậ<table></table></a></li>
+                        <li class="term-items"><a href="#" class="term-link">Điều khoản</a></li>
+                    </ul>
+                </div>
+                <div class="term-box">
+                    <h2 class="term-title">Công ty cổ phân công nghệ giáo dục Fullstack</h2>
+                    <ul class="term-list">
+                        <li class="term-items"><a href="#" class="term-link">Mã số thuế: 0109922901</a></li>
+                        <li class="term-items"><a href="#" class="term-link">Ngày thành lập: 04/03/2022</a></li>
+                        <li class="term-items"><a href="#" class="term-link">Lĩnh vực: Công nghệ, giáo dục, lập trình. Fullstack xây dựng và phát triển những sản phẩm mạng lại giá trị cho cộng đồng.</a></li>
+                    </ul>
+                </div>
+            </div>
+        </div>
+        <div class="footer-copyright">
+            <div class="copyright">&copy; 2018 - 2022 Fullstack. All rights reserved.</div>
+            <div class="more">
+                <ion-icon name="logo-youtube"></ion-icon>
+                <ion-icon name="logo-facebook"></ion-icon>
+                <ion-icon name="logo-github"></ion-icon>
+            </div>
+        </div>
     </div>
+</footer>
+</div>
 
-    <script type="text/javascript" src="//code.jquery.com/jquery-1.11.0.min.js"></script>
-    <script type="text/javascript" src="//code.jquery.com/jquery-migrate-1.2.1.min.js"></script>
-    <script type="text/javascript" src="slick/slick.min.js"></script>
-    <script src="./Assets/Js/app.js"></script>
+<script type="text/javascript" src="//code.jquery.com/jquery-1.11.0.min.js"></script>
+<script type="text/javascript" src="//code.jquery.com/jquery-migrate-1.2.1.min.js"></script>
+<script type="text/javascript" src="slick/slick.min.js"></script>
+<script src="./Assets/Js/app.js"></script>
 </body>
+
 </html>
